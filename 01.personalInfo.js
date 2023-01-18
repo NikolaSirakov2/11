3c7d09a2f9145deee6c21523cd5a284cb3692f6d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-18 23:07:02 +0200

    Create 01.personalInfo.js

---
+++ 01.personalInfo.js
@@ -0,0 +1,14 @@
+function personalInfo(firstName, lastName, age){
+    let human = {
+        first: firstName,
+        last: lastName,
+        age: age
+    }
+
+    console.log(human);
+
+    return human;
+}
+
+console.log(personalInfo("Nik","Sirakov","33"));
+
