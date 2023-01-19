f326d6a1ecc76932b5711f45187d316b20d0b0b2
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-19 12:14:07 +0200

    Update 05.cats.js

--- 05.cats.js
+++ 05.cats.js
@@ -1,15 +1,17 @@
-function cat(name, age){
+function cats(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let [name, age] = arr[i].split(" ");
+    
     let cat = {
-        name: name,
-        age: age,
-        sayMeow: function() {
-            return `${cat.name}, age ${cat.age} says Meow.`
-        }
-    }
+      name: name,
+      age: age,
+      sayMeow: function () {
+        return `${cat.name}, age ${cat.age} says Meow.`;
+      },
+    };
 
-    return cat;
+    console.log(cat.sayMeow());
+  }
 }
 
-let newCat = cat("Bibi", 2);
-
-console.log(newCat.sayMeow());
+cats(['Candy 1', 'Poppy 3', 'Nyx 2']);
