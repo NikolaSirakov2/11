a42dea519e4f182182e81c977fdb26003e93a9e1
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-19 12:48:36 +0200

    Create 001.CityRecord.js

---
+++ 001.CityRecord.js
@@ -0,0 +1,16 @@
+function cityRecord(name, population, treasury){
+    let record = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+    console.log(record); 
+    return record;
+}
+
+cityRecord('Tortuga',
+
+7000,
+
+15000)
