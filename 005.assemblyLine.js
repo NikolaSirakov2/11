74d2fdb7c23c6d811b4c7432bea5a06777d27ece
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-21 11:27:47 +0200

    Create 005.assemblyLine.js

---
+++ 005.assemblyLine.js
@@ -0,0 +1,46 @@
+function solve() {
+  return {
+    hasClima(car) {
+      car.temp = 21;
+      car.tempSettings = 21;
+      car.adjustTemp = function () {
+        if (car.temp > car.tempSettings) {
+          car.temp -= 1;
+        } else if (car.temp < car.tempSettings) {
+          car.temp += 1;
+        }
+      };
+    },
+
+    hasAudio(car) {
+        let song = car.currentTrack.name;
+        let artist =car.currentTrack.artist
+        car.nowPlaying = function() {
+            return `Now playing "${song}" by ${artist}`
+        }
+    }
+  };
+}
+
+const assemblyLine = solve();
+
+let car = {
+  make: "Fiat",
+  model: "Bravo",
+  
+};
+
+assemblyLine.hasClima(car);
+console.log(car.temp);
+car.tempSettings = 20;
+car.adjustTemp();
+
+console.log(car.temp);
+
+
+car.currentTrack = {
+        name: 'Never Gonna Give You Up',
+        artist: 'Rick Astley' 
+};
+assemblyLine.hasAudio(car);
+console.log(car.nowPlaying());
