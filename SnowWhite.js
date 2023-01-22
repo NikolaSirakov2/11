2aa2c624dd3300299cc0d6534088eeabfaa47328
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-22 19:47:03 +0200

    Update SnowWhite.js

--- SnowWhite.js
+++ SnowWhite.js
@@ -1,73 +1,88 @@
-function SnowWhite() {
-    this.name = "Snow White";
-    this.age = 0;
-    this.home = "Castle";
-    this.fairness = 0;
-    this.grow = function () {
+function Girl(name, age, home, fairness) {
+  this.name = name;
+  this.age = age;
+  this.home = home;
+  this.fairness = fairness;
+  this.grow = function () {
     this.age++;
-    this.fairness++
-    this.beg = false;
-    }
-    }
+    this.fairness++;
+  };
+}
 
-    function EvilQueen() {
-    this.name = "Queen";
-    this.age = 22;
-    this.fairness = 9;
-    this.askMirror = function (mirror) {
-    console.log("Mirror on the wall, who is the fairest of them all?")
-    console.log(mirror.whoIsTheFairest().name);
-    }
-    this.grow = function () {
+function Queen(name, age, home, fairness) {
+  this.name = name;
+  this.age = age;
+  this.home = home;
+  this.fairness = fairness;
+  this.askMirror = function (mirror) {
+    console.log("Mirror on the wall, who is the fairest of them all?");
+    console.log(`${mirror.whoIsTheFairest().name} is the fairest of all!`);
+  };
+  this.grow = function () {
     this.age++;
-    }
-    }
-
-    function Mirror() {
-    this.chicks = [];
-    this.whoIsTheFairest = function () {
-    return this.chicks.sort((a, b) => b.fairness - a.fairness)[0];
-    }
-    }
-
-    function Hunter() {
-        this.home = "Castle";
-        this.takeSomeone = function () {
-            snowWhite.home = "forest";
-            return "Hunter takes SnowWhite to the forest to kill her!";
-            
-        }
-        this.beMerciful = function () {
-            return "Hunter decide to let her go."
-        }
-    }
-
-    function Dwarf() {
-        this.name = undefined;
-        this.home = "dwarf house"
-    }
-
-    function Prince() {
-        this.look = "strong and beatiful";
-        this.position = "Castle";
-
-        this.inLove = false;
-        this.kiss = function () {
-            return "He kiss SnowWhite and she wake up!"
-        }
-    }
-
-    let snowWhite = new SnowWhite();
-    let evilQueen = new EvilQueen();
-    let mirror = new Mirror();
-    let hunter = new Hunter();
-
-   
-
+    this.fairness--;
+  };
+  this.commandToKill = function (hunter) {
+    console.log("Queen commands one man to kill SnowWhite!");
     console.log(hunter.takeSomeone());
+  };
+}
 
-    if (snowWhite.home === "forest"){
-        console.log("She beg him for mercy!");
-        console.log(hunter.beMerciful());
-        snowWhite.home = "dwarf house";
-    }
+function Mirror(chicks) {
+  this.chicks = chicks;
+  this.whoIsTheFairest = function () {
+    return this.chicks.sort((a, b) => b.fairness - a.fairness)[0];
+  };
+}
+
+function Hunter() {
+  this.home = "Castle";
+  this.takeSomeone = function () {
+    snowWhite.home = "forest";
+    return "Hunter takes SnowWhite in the forest to kill her!";
+  };
+  this.beMerciful = function () {
+    return "He decide to let her go.";
+  };
+}
+
+function Dwarf() {
+  this.name = undefined;
+  this.home = "dwarf house";
+}
+
+function Prince() {
+  this.look = "strong and beatiful";
+  this.position = "Castle";
+
+  this.inLove = false;
+  this.kiss = function () {
+    return "He kiss SnowWhite and she wake up!";
+  };
+}
+
+let snowWhite = new Girl("SnowWhite", 2, "Castle", 1);
+let evilQueen = new Queen("Queen", 22, "Castle", 11);
+let mirror = new Mirror([snowWhite, evilQueen]);
+let hunter = new Hunter();
+
+// console.log(snowWhite);
+// console.log(evilQueen);
+// console.log(mirror);
+// console.log(hunter);
+
+while (snowWhite.fairness <= evilQueen.fairness) {
+  evilQueen.grow();
+  snowWhite.grow();
+  evilQueen.askMirror(mirror);
+}
+
+if (snowWhite.fairness > evilQueen.fairness) {
+    evilQueen.commandToKill(hunter)
+
+  if (snowWhite.home === "forest") {
+    console.log("She beg him for mercy!");
+    console.log(hunter.beMerciful());
+    snowWhite.home = "dwarf house";
+  }
+}
