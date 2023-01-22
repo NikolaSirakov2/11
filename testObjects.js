dcd4026a659ce1f0b921be5f5ec0e8de851caa08
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-22 19:47:07 +0200

    Update testObjects.js

--- testObjects.js
+++ testObjects.js
@@ -5,3 +5,30 @@ let obj = {
     }
 }
 console.log(obj.sayHello());
+
+function Hunter() {
+    this.home = "Castle";
+    this.takeSomeone = function () {
+        snowWhite.home = "forest";
+        return "Hunter takes SnowWhite to the forest to kill her!";
+        
+    }
+    this.beMerciful = function () {
+        return "Hunter decide to let her go."
+    }
+}
+
+function Dwarf() {
+    this.name = undefined;
+    this.home = "dwarf house"
+}
+
+function Prince() {
+    this.look = "strong and beatiful";
+    this.position = "Castle";
+
+    this.inLove = false;
+    this.kiss = function () {
+        return "He kiss SnowWhite and she wake up!"
+    }
+}
