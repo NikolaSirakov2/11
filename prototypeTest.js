89bda3c21c57d12530ae58ce76a85f28a9ecd47d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-22 11:40:44 +0200

    Update prototypeTest.js

--- prototypeTest.js
+++ prototypeTest.js
@@ -3,8 +3,11 @@ function Hero (name, power){
     this.power = power;
 }
 
+console.log(Hero.prototype);
+
 Hero.prototype.age = 300;
 
 let superman = new Hero ("Superman", "fly");
 
 console.log(superman);
+console.log(superman.__proto__);
