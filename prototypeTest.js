99a1c4d2887ea934284b43d1689c32ec25bfdb91
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-22 13:52:18 +0200

    Update prototypeTest.js

--- prototypeTest.js
+++ prototypeTest.js
@@ -1,28 +1,13 @@
-// function Hero (name, power){
-//     this.name = name;
-//     this.power = power;
-// }
+function Hero (name, power){
+    this.name = name;
+    this.power = power;
+}
 
-// console.log(Hero.prototype);
+console.log(Hero.proto);
 
-// Hero.prototype.age = 300;
+Hero.prototype.age = 300;
 
-// let superman = new Hero ("Superman", "fly");
+let superman = new Hero ("Superman", "fly");
 
-// console.log(superman);
-// console.log(superman.__proto__);
-
-let fullname = "John Doe";
-let obj = {
-  fullname: "Colin Ihrig",
-  prop: {
-    fullname: "Aurelio De Rosa",
-    getFullname: function () {
-      return this.fullname;
-    },
-  },
-};
-console.log(obj.prop.getFullname());
-let test = obj.prop.getFullname;
-
-console.log(test());
+console.log(superman);
+console.log(superman.__proto__);
