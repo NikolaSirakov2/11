2b0fa07f05562926dc1ece53cabd152806ec1aae
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-23 21:15:02 +0200

    Create 2.ConstructionCrew.js

---
+++ SoftUniExercises-20.09/2.ConstructionCrew.js
@@ -0,0 +1,22 @@
+function crew({weight, experience, levelOfHydrated, dizziness}){
+    let worker ={
+        weight: weight,
+        experience: experience,
+        levelOfHydrated: levelOfHydrated,
+        dizziness: dizziness
+    }
+
+    if(worker.dizziness === true){
+        worker.levelOfHydrated += (0.1*worker.weight*worker.experience);
+    }
+
+    console.log(worker);
+}
+
+crew({ weight: 95,
+
+    experience: 3,
+    
+    levelOfHydrated: 0,
+    
+    dizziness: false })
