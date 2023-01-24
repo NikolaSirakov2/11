11027eec674a5581d9158b9c8a63c572e81bf662
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-24 10:48:10 +0200

    Update 4.HeroicInventory.js

--- SoftUniExercises-20.09/4.HeroicInventory.js
+++ SoftUniExercises-20.09/4.HeroicInventory.js
@@ -1,7 +1,21 @@
 function inventory(arr){
-    let obj = {};
+    let obj = {
+        heroName: undefined,
+        heroLevel: undefined,
+        items: undefined
+    };
 
+    
 
+    for(let i = 0; i < arr.length; i++){
+        let info = arr[i].split(" / ");
+
+        obj.heroName = info[0];
+        obj.heroLevel = Number(info[1]);
+        obj.items = info[2].split(", ");
+    }
+
+    console.log(JSON.stringify(obj));
 }
 
-inventory(['Jake / 1000 / Gauss, HolidayGrenade'])
+inventory(['Jake / 1000 / Gauss, HolidayGrenade']);
