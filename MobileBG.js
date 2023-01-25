d4a24895908b8b2704707dc92ee6a7c167d6d071
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-25 21:36:39 +0200

    Update MobileBG.js

--- MobileBG.js
+++ MobileBG.js
@@ -1,6 +1,16 @@
 class User {
     constructor (name, password) {
         this.name = name;
-        this.password = password
+        this.password = password;
     }
 }
+
+let gosho = new User("Gosho", "007")
+
+class UserManager {
+    constructor (User){
+        this.user = User.name;
+    }
+}
+
+console.log(gosho);
