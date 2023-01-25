004b367bd3340023af22d18424f6b8ecac976d3b
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-25 21:32:06 +0200

    Update 6.StorageCatalog.js

--- SoftUniExercises-20.09/6.StorageCatalog.js
+++ SoftUniExercises-20.09/6.StorageCatalog.js
@@ -1,30 +1,28 @@
 function catalog(arr) {
-  console.log(arr);
-
   let sorted = arr.sort();
-
-  console.log(sorted);
 
   let catalog = {};
 
-  for(let i = 0; i < sorted.length; i++){
-        if(!catalog.hasOwnProperty(sorted[i][0])){
-            catalog[sorted[i][0]] = sorted[i];
-        } else {
-            catalog[sorted[i][0]] += sorted[i];
-        }
+  for (let i = 0; i < sorted.length; i++) {
+    let el = sorted[i].split(" : ");
+    
+    if (!catalog.hasOwnProperty(sorted[i][0])) {
+      catalog[sorted[i][0]] = sorted[i];
+      console.log(sorted[i][0]);
+      console.log("  " + el[0] + ": " + el[1]);
+    } else {
+        console.log("  " + el[0] + ": " + el[1]);
+    }
   }
-
-  console.log(catalog);
 }
 
 catalog([
-  "Appricot : 20.4",
-  "Fridge : 1500",
-  "TV : 1499",
-  "Deodorant : 10",
-  "Boiler : 300",
-  "Apple : 1.25",
-  "Anti-Bug Spray : 15",
-  "T-Shirt : 10",
+  "Banana : 2",
+  "Rubic`s Cube : 5",
+  "Raspberry P : 4999",
+  "Rolex : 100000",
+  "Rollon : 10",
+  "Rali Car : 2000000",
+  "Pesho : 0.000001",
+  "Barrel : 10",
 ]);
