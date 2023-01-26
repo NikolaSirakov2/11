f6960b0c22f7eb848f4d998f69d55d4a019965c0
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-26 07:38:24 +0200

    Update MobileBG.js

--- MobileBG.js
+++ MobileBG.js
@@ -1,39 +1,40 @@
 class User {
-    constructor (name, password, email) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
+  constructor(name, password, email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
 }
 
 class UserManager {
+  allUsers = [];
+  loggedUsers = [];
 
-    allUsers = [];
-    loggedUsers = [];
-    constructor (User){
-        this.user = User.name;
+  constructor() {};
+
+  createNewUser(user) {
+    if (!this.allUsers.hasOwnProperty(user.name)) {
+      console.log("Create new user!");
+      this.allUsers.push(user);
     }
-
-    createNewUser() {
-
-    }
+  }
 }
 
 class Vehicle {
-    constructor(made, type){
-        this.made = made;
-        this.type = type;
-    }
+  constructor(made, type) {
+    this.made = made;
+    this.type = type;
+  }
 }
 
 class Car extends Vehicle {
-    constructor(made, type, kilometers, maxSpeed){
-        super (Vehicle);
-        this.kilometers = kilometers;
-        this.maxSpeed = maxSpeed;
-    }
+  constructor(made, type, kilometers, maxSpeed) {
+    super(Vehicle);
+    this.kilometers = kilometers;
+    this.maxSpeed = maxSpeed;
+  }
 }
 
+let userManager = new UserManager;
 
-
-console.log(new Car("BMW", "jeep", 20000, 250));
+userManager.createNewUser(new User("Nik", "parola", "test@abv.bg"))
