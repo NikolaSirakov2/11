f0693fe8d3495f528fc7082cbd832734671b0e15
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-28 11:12:45 +0200

    Update MobileBgModele.js

--- MobileBgModele.js
+++ MobileBgModele.js
@@ -1,70 +1,76 @@
 class User {
-    constructor(username,password,email){
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
+  constructor(username, password, email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
 }
 
 class UserManager {
-    users = [];
-    logedUser = null;
+  users = [];
+  logedUser = null;
 
-    addUser (newUser){
-        let filter = this.users.filter(i => i.name === newUser.name);
+  addUser(newUser) {
+    let filter = this.users.filter((i) => i.name === newUser.name);
 
-        if(filter.length){
-            console.log("This username is already taken!");
-        } else {
-            this.users.push(newUser);
-        }
+    if (filter.length) {
+      console.log("This username is already taken!");
+    } else {
+      this.users.push(newUser);
     }
+  }
 
-    deleteUser (user){
-        let filter = this.users.filter(i => i.name !== user.name);
+  deleteUser(user) {
+    let filter = this.users.filter((i) => i.name !== user.name);
 
-        this.users = filter;
+    this.users = filter;
+  }
+
+  loginUser(user) {
+    let filter = this.users.filter((i) => i.name === user.name);
+
+    if (filter.length && this.logedUser === null) {
+      this.logedUser = user;
+    } else if (filter.length <= 0) {
+      console.log("Wrong username!");
     }
+  }
 
-    loginUser (user){
-        let filter = this.users.filter(i => i.name === user.name);
-
-        if(filter.length && this.logedUser === null){
-            this.logedUser = user;
-        } else if (filter.length <= 0) {
-            console.log("Wrong username!");
-        }
-    }
-
-    logoutUser (){
-        this.logedUser = null;
-    }
+  logoutUser() {
+    this.logedUser = null;
+  }
 }
 
 class Offer {
-    constructor(title, price, photos, description, vehicle){
-        this.title = title;
-        this.price = price;
-        this.photos = photos;
-        this.description = description;
-        this.vehicle = vehicle;
-    }
+  constructor(title, price, photos, description, vehicle) {
+    this.title = title;
+    this.price = price;
+    this.photos = photos;
+    this.description = description;
+    this.vehicle = vehicle;
+  }
 }
 
 class offerManager {
-    offers = [];
+  offers = [];
 
-    createOffer(offer) {
-        let filter = this.offers.filter(i => i.name === offer.name);
+  createOffer(offer) {
+    let filter = this.offers.filter((i) => i.name === offer.name);
 
-        if(filter.length){
-            console.log("This offer is already added!");
-        } else {
-            this.offers.push(offer);
-        }
+    if (filter.length) {
+      console.log("This offer is already added!");
+    } else {
+      this.offers.push(offer);
     }
+  }
 
-    deleteOffer(offer) {
-        let filter = of
-    }
+  deleteOffer(offer) {
+    let filter = this.offers.filter((i) => i.name !== offer.name);
+
+    this.offers = filter;
+  }
+
+  editOffer(offer, property, newValue) {}
+
+  filterOffers(k)
 }
