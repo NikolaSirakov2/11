51a47298f4973525b6992daacd0b3afaa3182d15
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-29 14:24:04 +0200

    Update copyObject.js

--- copyObject.js
+++ copyObject.js
@@ -12,3 +12,16 @@ person.dog = true;
 
 console.log(person);
 console.log(newPerson);
+
+editOffer(property, newValue) {
+    let edit = this.offers.filter(function(a) {
+        return a[property] = newValue;
+    })
+}
+
+
+if(search = []){
+    console.log("There is no offer with this name!");
+} else {
+    console.log(search[0]);
+}
